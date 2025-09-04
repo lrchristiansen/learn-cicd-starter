9b0778fc93e8cb56077d98743753029f25fc1f15
lrchristiansen <laura.r.christiansen@gmail.com>
2025-09-03 21:13:24 -0500

    Please

--- main.go
+++ main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"database/sql"
 	"embed"
-	"github.com/bootdotdev/learn-cicd-starter/internal/database"
-	"github.com/go-chi/chi"
-	"github.com/go-chi/cors"
-	"github.com/joho/godotenv"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+
+	"github.com/bootdotdev/learn-cicd-starter/internal/database"
 
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
